e7bd179fa42b9e79477462fa9dc1c8f2d809b6c7
jack <1484288157@qq.com>
2018-12-17 11:36:40 +0800

    列表刷新

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -9,6 +9,7 @@ import 'package:wanandroid/net/dio_manager.dart';
 import 'package:wanandroid/ui/login_page.dart';
 import 'package:wanandroid/utils/color.dart';
 import 'package:wanandroid/utils/textsize.dart';
+import "package:pull_to_refresh/pull_to_refresh.dart";
 
 class HomePage extends StatefulWidget {
   @override
@@ -23,13 +24,15 @@ class _HomePageState extends State<HomePage>
   SwiperController _controller = SwiperController();
   int pageIndex = 0;
   List<Article> articles = List();
+  RefreshController _refreshController;
 
   @override
   void initState() {
     super.initState();
     _controller.autoplay = true;
+    _refreshController = new RefreshController();
     getBanner();
-    getList();
+    getList(true);
   }
 
   void getBanner() async {
@@ -42,80 +45,94 @@ class _HomePageState extends State<HomePage>
     });
   }
 
+  void _onRefresh(bool up) {
+    if (up) {
+      getBanner();
+      pageIndex = 0;
+      getList(true);
+    } else {
+      pageIndex++;
+      getList(false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: articles.length + 1,
-        itemBuilder: (context, index) {
-          return index == 0
-              ? Container(
-            margin: EdgeInsets.only(top: 5),
-            width: MediaQuery.of(context).size.width,
-            height: 180,
-            child: banners.length != 0
-                ? Swiper(
-              controller: _controller,
-              itemWidth: MediaQuery.of(context).size.width,
-              itemHeight: 180,
-              pagination: pagination(),
-              itemBuilder: (BuildContext context, int index) {
-                return new Image.network(
-                  banners[index].imagePath,
-                  fit: BoxFit.fill,
-                );
-              },
-              itemCount: banners.length,
-              viewportFraction: 0.8,
-              scale: 0.9,
-            )
-                : SizedBox(
-              width: 0,
-              height: 0,
-            ),
-          )
-              : _builditem(index - 1);
-        });
+    return SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: true,
+        onRefresh: _onRefresh,
+        controller: _refreshController,
+        child: ListView.builder(
+            itemCount: articles.length + 1,
+            itemBuilder: (context, index) {
+              return index == 0
+                  ? Container(
+                      margin: EdgeInsets.only(top: 5),
+                      width: MediaQuery.of(context).size.width,
+                      height: 180,
+                      child: banners.length != 0
+                          ? Swiper(
+                              controller: _controller,
+                              itemWidth: MediaQuery.of(context).size.width,
+                              itemHeight: 180,
+                              pagination: pagination(),
+                              itemBuilder: (BuildContext context, int index) {
+                                return new Image.network(
+                                  banners[index].imagePath,
+                                  fit: BoxFit.fill,
+                                );
+                              },
+                              itemCount: banners.length,
+                              viewportFraction: 0.8,
+                              scale: 0.9,
+                            )
+                          : SizedBox(
+                              width: 0,
+                              height: 0,
+                            ),
+                    )
+                  : _builditem(index - 1);
+            }));
   }
 
   SwiperPagination pagination() => SwiperPagination(
       margin: EdgeInsets.all(0.0),
       builder: SwiperCustomPagination(
           builder: (BuildContext context, SwiperPluginConfig config) {
-            return Container(
-              color: Color(0x599E9E9E),
-              height: 40,
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: Row(
-                children: <Widget>[
-                  Text(
-                    "${banners[config.activeIndex].title}",
-                    style: TextStyle(
-                        fontSize: TextSizeConst.smallTextSize,
-                        color: ColorConst.color_white),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: new Align(
-                      alignment: Alignment.centerRight,
-                      child: new DotSwiperPaginationBuilder(
+        return Container(
+          color: Color(0x599E9E9E),
+          height: 40,
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Row(
+            children: <Widget>[
+              Text(
+                "${banners[config.activeIndex].title}",
+                style: TextStyle(
+                    fontSize: TextSizeConst.smallTextSize,
+                    color: ColorConst.color_white),
+              ),
+              Expanded(
+                flex: 1,
+                child: new Align(
+                  alignment: Alignment.centerRight,
+                  child: new DotSwiperPaginationBuilder(
                           color: Colors.black12,
                           activeColor: ColorConst.color_primary,
                           size: 6.0,
                           activeSize: 6.0)
-                          .build(context, config),
-                    ),
-                  )
-                ],
-              ),
-            );
-          }));
+                      .build(context, config),
+                ),
+              )
+            ],
+          ),
+        );
+      }));
 
   Widget _builditem(int index) {
     Article article = articles[index];
     return GestureDetector(
-      onTap: (){
-
-      },
+      onTap: () {},
       child: Card(
         margin: EdgeInsets.all(5),
         child: Container(
@@ -170,17 +187,20 @@ class _HomePageState extends State<HomePage>
                 children: <Widget>[
                   !article.collect
                       ? IconButton(
-                    alignment: Alignment.centerLeft,
-                    padding: EdgeInsets.all(0),
-                    icon: Icon(Icons.favorite_border),
-                    onPressed: () => _collect(index),
-                  )
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.all(0),
+                          icon: Icon(Icons.favorite_border),
+                          onPressed: () => _collect(index),
+                        )
                       : IconButton(
-                    alignment: Alignment.centerLeft,
-                    padding: EdgeInsets.all(0),
-                    icon: Icon(Icons.favorite,color: Colors.red,),
-                    onPressed: () => _collect(index),
-                  ),
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.all(0),
+                          icon: Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          onPressed: () => _collect(index),
+                        ),
                   Icon(
                     Icons.access_time,
                     color: ColorConst.color_ccc,
@@ -212,18 +232,21 @@ class _HomePageState extends State<HomePage>
     );
   }
 
-  void getList() {
+  void getList(bool isRefresh) {
     DioManager.singleton.get("article/list/${pageIndex}/json").then((result) {
-      BaseListData listdata = BaseListData.fromJson(result.data);
-      if (pageIndex == 0) {
-        articles.clear();
-      }
-      setState(() {
-        for (var a in listdata.datas) {
-          Article article = Article.fromJson(a);
-          articles.add(article);
+      _refreshController.sendBack(isRefresh, RefreshStatus.idle);
+      if (result != null) {
+        BaseListData listdata = BaseListData.fromJson(result.data);
+        if (pageIndex == 0) {
+          articles.clear();
         }
-      });
+        setState(() {
+          for (var a in listdata.datas) {
+            Article article = Article.fromJson(a);
+            articles.add(article);
+          }
+        });
+      }
     });
   }
 
@@ -232,14 +255,14 @@ class _HomePageState extends State<HomePage>
 
   _collect(int index) {
     Article article = articles[index];
-    String url="";
-    if(!article.collect){
-      url="lg/collect/${article.id}/json";
-    }else{
-      url="lg/uncollect_originId/${article.id}/json";
+    String url = "";
+    if (!article.collect) {
+      url = "lg/collect/${article.id}/json";
+    } else {
+      url = "lg/uncollect_originId/${article.id}/json";
     }
-    DioManager.singleton.post(url).then((result){
-      if(result!=null){
+    DioManager.singleton.post(url).then((result) {
+      if (result != null) {
         setState(() {
           article.collect = !article.collect;
         });
